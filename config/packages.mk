b1175655ae9ebb79f5b3e781ae5eb076d41821d8
Dyneteve <dyneteve@hentaios.com>
2023-11-17 21:08:42 +0530

    config: Build the textclassifier models
    Change-Id: I9d7d582ddc427998e68e668cc0375958325d8052
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -52,4 +52,11 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     charger_res_images_vendor_pixel
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_annotator_en_model \
+    libtextclassifier_annotator_universal_model \
+    libtextclassifier_actions_suggestions_universal_model \
+    libtextclassifier_lang_id_model
+
 -include vendor/statix/config/overlay.mk

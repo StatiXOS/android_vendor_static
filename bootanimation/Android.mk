e84b5a5be9a67e852365eeb39e5457d44e1abffc
Pranaya Deomani <pranayadmn@proton.me>
2024-05-03 16:07:34 +0530

    statix: Refactor boot animation installation logic
    * Simplify installation logic by converting it into a module and making
      boot animation resolution configurable using `BOOT_ANIMATION_RES` flag,
      with 1080p acting as the default if undefined.
    * Install it to /product to support switching between light and dark boot
      animations in the future and to get rid of artifact path requirement.
    
    Change-Id: Id26e4eff9d6bf7525a4357bcfcb9bfd37b988816
    Signed-off-by: Pranaya Deomani <pranayadmn@proton.me>

---
+++ bootanimation/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bootanimation.zip
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/media
+LOCAL_SRC_FILES := $(BOOT_ANIMATION_RES).zip
+include $(BUILD_PREBUILT)

1e9fe0655e8919686c45e99f7df40dbbc705b9ef
Pranaya Deomani <pranayadmn@proton.me>
2024-09-04 12:24:59 +0530

    config: Disable one-handed mode for tablets
    Change-Id: Ia87e96e5258373794a6e61de3bcc92810c76a6dd

--- config/tablet.mk
+++ config/tablet.mk
@@ -4,6 +4,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
 
+# One-handed mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode=false
+
 # Settings
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.settings.large_screen_opt.enabled=true

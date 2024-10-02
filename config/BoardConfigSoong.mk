7ddac6f1f9d83d1a78290f5c08c13cfb6ddae253
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-02 08:43:13 -0400

    statix: Move qcom specific stuff to hardware/qcom-caf [1/2]
    Change-Id: I32c05b207691c318a36862a5bbe51be5b93a9e74

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -17,28 +17,6 @@ SOONG_CONFIG_statixGlobalVars += \
     target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib
 
-SOONG_CONFIG_NAMESPACES += statixQcomVars
-SOONG_CONFIG_statixQcomVars += \
-    qti_vibrator_effect_lib \
-    qti_vibrator_use_effect_stream \
-    should_wait_for_qsee \
-    supports_extended_compress_format \
-    uses_pre_uplink_features_netmgrd \
-    uses_qti_camera_device
-
-# Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
-ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
-SOONG_CONFIG_statixQcomVars += \
-    qcom_display_headers_namespace
-endif
-
-# Soong bool variables
-SOONG_CONFIG_statixQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
-SOONG_CONFIG_statixQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
-SOONG_CONFIG_statixQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
-SOONG_CONFIG_statixQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
-SOONG_CONFIG_statixQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
-
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_ENABLED ?= 1
@@ -48,7 +26,6 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE ?= false
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE ?= true
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB ?= libperfmgr-ext
-TARGET_QTI_VIBRATOR_EFFECT_LIB ?= libqtivibratoreffect
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
@@ -68,13 +45,3 @@ SOONG_CONFIG_statixGlobalVars_target_health_charging_control_stage_path := $(TAR
 SOONG_CONFIG_statixGlobalVars_target_health_charging_control_supports_bypass := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS)
 SOONG_CONFIG_statixGlobalVars_target_health_charging_control_supports_deadline := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE)
 SOONG_CONFIG_statixGlobalVars_target_health_charging_control_supports_toggle := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE)
-ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
-SOONG_CONFIG_statixQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
-else
-SOONG_CONFIG_statixQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
-endif
-SOONG_CONFIG_statixQcomVars_qti_vibrator_effect_lib := $(TARGET_QTI_VIBRATOR_EFFECT_LIB)
-
-ifneq ($(TARGET_USE_QTI_BT_STACK),true)
-PRODUCT_SOONG_NAMESPACES += packages/apps/Bluetooth
-endif #TARGET_USE_QTI_BT_STACK

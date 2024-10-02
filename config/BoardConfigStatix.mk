0f2d6a6ffca22ec2d3ed41611eef55f2c0dd647c
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-02 08:43:13 -0400

    statix: Move qcom specific stuff to hardware/qcom-caf [1/2]
    Change-Id: I32c05b207691c318a36862a5bbe51be5b93a9e74
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- config/BoardConfigStatix.mk
+++ config/BoardConfigStatix.mk
@@ -1,8 +1,6 @@
-ifeq ($(TARGET_USE_QTI_BT_STACK),true)
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
-    vendor/qcom/opensource/commonsys/system/bt/conf
-endif #TARGET_USE_QTI_BT_STACK
-
 include device/statix/sepolicy/common/sepolicy.mk
 include vendor/statix/config/BoardConfigSoong.mk
+
+ifeq ($(PRODUCT_USES_QCOM_HARDWARE), true)
+include hardware/qcom-caf/common/BoardConfigSoong.mk
+endif

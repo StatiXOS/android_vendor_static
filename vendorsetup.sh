f537df571e0d7194b7ff547ceaadda2b326eff64
Dyneteve <dyneteve@hentaios.com>
2025-01-24 10:48:38 +0100

    vendorsetup: Override build number
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>
    Change-Id: I7d03a4aad6a87eeb2826160514a1e6ea250418e8

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -26,3 +26,6 @@ echo -e "\nDon't forget to join https://t.me/StatiXOSReleases! Enjoy! :D"
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_USERNAME=nobody
 export BUILD_HOSTNAME=android-build
+
+# Override build number
+export BUILD_NUMBER=$(date +%y%m%d%S)

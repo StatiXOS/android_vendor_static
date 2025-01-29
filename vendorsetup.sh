8dc64720386740f6a159d8ab5815cd0ae8c1b750
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2025-01-28 22:05:03 -0500

    vendorsetup: Only show full welcome message first time
    Change-Id: I67c8e9fdd2287553a88193ebbfd9ac07bc08e253
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/15158
    Reviewed-by: Vaisakh Murali <mvaisakh@statixos.com>
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,24 +4,34 @@
 YELLOW='\033[1;33m'
 RESET='\033[0m'
 
-# Welcome message
-echo -e "Welcome to ${YELLOW}StatiX!${RESET}"
+# Check if the welcome message has already been shown
+if [ -z "${WELCOME_SHOWN}" ]; then
 
-# Additional instructions
-echo -e "\nHere are some things before you get started!\n"
-echo -e "- Make sure you have these variables highlighted in this link in your device tree:"
-echo -e "  https://github.com/StatiXOS/android_device_xiaomi_sky/blob/udc/device.mk#L11-L14"
+    # Welcome message
+    echo -e "Welcome to ${YELLOW}StatiX!${RESET}"
 
-echo -e "\n- Next, make sure you have a prebuilt kernel repo set up. It should look something like this:"
-echo -e "  https://github.com/StatiXOS/android_device_xiaomi_sky-kernel"
+    # Additional instructions
+    echo -e "\nHere are some things before you get started!\n"
+    echo -e "- Make sure you have these variables highlighted in this link in your device tree:"
+    echo -e "  https://github.com/StatiXOS/android_device_xiaomi_sky/blob/udc/device.mk#L11-L14"
 
-echo -e "\n- If you don't have a prebuilt kernel repo, follow the manifests we have here for other devices and adapt it to yours:"
-echo -e "  https://github.com/StatiXOS/android_kernel_manifest"
+    echo -e "\n- Next, make sure you have a prebuilt kernel repo set up. It should look something like this:"
+    echo -e "  https://github.com/StatiXOS/android_device_xiaomi_sky-kernel"
 
-echo -e "\n- Finally, push any changes to our Gerrit if you think they're worthwhile, and we will try to review it as soon as possible.\n  Follow the guide below to get set up with our Gerrit:"
-echo -e "  https://github.com/StatiXOS/android_manifest?tab=readme-ov-file#submitting-patches"
+    echo -e "\n- If you don't have a prebuilt kernel repo, follow the manifests we have here for other devices and adapt it to yours:"
+    echo -e "  https://github.com/StatiXOS/android_kernel_manifest"
 
-echo -e "\nDon't forget to join https://t.me/StatiXOSReleases! Enjoy! :D"
+    echo -e "\n- Finally, push any changes to our Gerrit if you think they're worthwhile, and we will try to review it as soon as possible.\n  Follow the guide below to get set up with our Gerrit:"
+    echo -e "  https://github.com/StatiXOS/android_manifest?tab=readme-ov-file#submitting-patches"
+
+    echo -e "\nDon't forget to join https://t.me/StatiXOSReleases! Enjoy! :D"
+
+    export WELCOME_SHOWN=true
+
+else
+    echo -e "Welcome to ${YELLOW}StatiX!${RESET}"
+    echo -e "Environment ready."
+fi
 
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_USERNAME=nobody
